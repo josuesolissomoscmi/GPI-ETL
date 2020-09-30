87e8d56e80f5e74cd668c58ed6e8f48d5d7b3c68
autom <autom@CMIAZSRVML01.cmi.local>
2020-09-30 16:00:53 -0600

    Json response Nex

--- HTIPNEXSITE/__init__.py
+++ HTIPNEXSITE/__init__.py
@@ -6,6 +6,8 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    response = '{"Forecast":"A"}'
+
     name = req.params.get('name')
     if not name:
         try:
@@ -16,7 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"A")
+        return func.HttpResponse(response)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
